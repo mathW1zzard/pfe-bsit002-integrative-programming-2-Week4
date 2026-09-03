f610755d1a22533deec784f139e2c16f0c9f94e7
Ha1lH1tl3r <dionisiojoseph213@gmail.com>
2026-09-04 07:09:54 +0800

    Week 4

---
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee; // <--
+use Illuminate\Http\Request;
+
+
+class EmployeeController extends Controller
+{
+    // List of all Employee
+    public function index()
+    {
+        return response()->json(Employee::all());
+    }
+
+    // POST
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+
+            'first_name' => 'required|string|max:100',
+            'last_name' => 'required|string|max:100',
+            'email' => 'required|email|unique:employees,email',
+            'department' => 'required|string|max:100',
+            'position' => 'required|string|max:50',
+        ]);
+
+        $employee = Employee::create($validated);
+
+        return response()-> json($employee, 201);
+    }
+
+    // GET
+    public function show($id)
+    {
+        $employee = Employee::file($id);
+
+        if(!$employee) {
+            return response()-> json(['message' => 'Empoyee not found'], 404);
+        }
+
+        return response()-> json($employee);
+
+    }
+
+    // PUT
+    public function update(Request $request, $id)
+    {
+        $employee = Employee::find($id);
+
+        if(!$employee) {
+            return response()->json(['message' => 'Employee not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'first_name' => 'sometimes|string|max:100',
+            'last_name' => 'sometimes|string|max:100',
+            'email' => 'sometimes|email|unique:employees,email,' . $id,
+            'department' => 'sometimes|string|max:100',
+            'position' => 'sometimes|string|max:100',
+        ]);
+
+        $employee->update($validated);
+        return response()->json($employee);
+    }
+
+    // DELETE
+    public function destroy($id)
+    {
+    $employee = Employee::find($id);
+
+    if (!$employee) {
+        return response()->json(['message' => 'Employee not found'], 404);
+    }
+
+    $employee->delete();
+
+    return response()->json(['message' => 'Employee deleted successfully']);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EmployeeController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::post('/employees', [EmployeeController::class, 'store']);
+Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
